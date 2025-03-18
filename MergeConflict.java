3f94512c06390b2d54b009b3ad61a1da12f60cc6
Boddu Venkata Nikhil Reddy <bodduvenkatanikhil_reddy@epam.com>
2025-03-18 16:30:52 +0530

    Gayathri's Commit in development

--- MergeConflict.java
+++ MergeConflict.java
@@ -1,5 +1,5 @@
 public class MergeConflict {
     public static void main(String[] args) {
-        System.out.println("Hii this is Nikhil");
+        System.out.println("Hii this is Gayathri");
     }
 }
